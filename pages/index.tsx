a941efd6f50d2a963f08b9bd34e8ccdb9924b41d
kex5n <kentaro.ishii0407@gmail.com>
2024-01-08 22:48:27 +0900

    initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,47 @@
+import { GetServerSideProps, GetServerSideProps, NextPage } from "next";
+import { useState } from "react";
+import styles from './index.module.css'
+
+type Props = {
+    initialImageUrl: string;
+}
+
+const IndexPage: NextPage<Props> = ({ initialImageUrl }) => {
+    const [imageUrl, setImageUrl] = useState(initialImageUrl);
+    const [loading, setLoading] = useState(false);
+    const handleClick = async () => {
+        setLoading(true);
+        const newImage = await fetchImage();
+        setImageUrl(newImage.url);
+        setLoading(false);
+    }
+    return (
+        <div className={styles.page}>
+            <button onClick={handleClick}>他のにゃんこも見る</button>
+            <div className={styles.frame}>
+                {loading || <img src={imageUrl} />}
+            </div>
+        </div>
+    )
+}
+export default IndexPage;
+
+type Image = {
+    url: string;
+}
+
+export const getServerSideProps: GetServerSideProps<Props> = async () => {
+    const image = await fetchImage();
+    return {
+        props: {
+            initialImageUrl: image.url,
+        }
+    }
+}
+
+const fetchImage = async (): Promise<Image> => {
+    const res = await fetch("https://api.thecatapi.com/v1/images/search");
+    const images = await res.json();
+    console.log(images);
+    return images[0];
+};
